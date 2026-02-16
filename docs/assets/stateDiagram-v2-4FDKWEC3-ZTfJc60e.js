54dd3876efbf11c5413bdf1e5cd2664b28e469c0
jSchmidt-design <jakob_1@gmx.de>
2026-02-16 18:12:45 +0100

    added Version Warning

--- docs/assets/stateDiagram-v2-4FDKWEC3-ZTfJc60e.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-57l693uO.js";import{_ as i}from"./index-CGWdr0WL.js";import"./chunk-55IACEB6-BRJqjGRz.js";import"./chunk-QN33PNHL-BfAw_G72.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
