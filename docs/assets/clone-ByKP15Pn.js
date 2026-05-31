635c579efbd44a80d82f4d90443c41ebb07c2c64
jSchmidt-design <jakob_1@gmx.de>
2026-05-31 13:38:02 +0200

    0.15.0 Release

---
+++ docs/assets/clone-ByKP15Pn.js
@@ -0,0 +1,1 @@
+import{b as r}from"./graph-DdWZ2N4y.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ docs/assets/stateDiagram-v2-4FDKWEC3-fEi7DOvM.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-BndgttL3.js";import{_ as i}from"./index-BkoFGXqr.js";import"./chunk-55IACEB6-hLbb2YF4.js";import"./chunk-QN33PNHL-B47xAxed.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
